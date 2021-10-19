0ebd3279ae11ae0ae39c707c1a69db580693a371
adbartni <abartnik@bnac.net>
2021-10-19 10:12:57 -0400

    Getting ready to finalize acquisition ontology

--- src/ontology/generate_update_query.sh
+++ src/ontology/generate_update_query.sh
@@ -7,9 +7,6 @@ tr=$4
 ti=$5
 
 cat > ../sparql/update_query.ru << EOF
-PREFIX owl: <http://www.w3.org/2002/07/owl#>
-PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
 PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
 PREFIX obo: <http://purl.obolibrary.org/obo/>
 
@@ -22,3 +19,4 @@ INSERT DATA
                          obo:MRIO_0000606 "$ti"^^xsd:float .
 }
 EOF
+
---
+++ src/ontology/get_sequence_from_parameters.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+id=$1
+fa=$2
+te=$3
+tr=$4
+ti=$5
+
+## Create SPARQL UPDATE query with IRI = MRIO_000$id
+## and specified acquisition parameters;
+## UPDATE query saved to ../sparql/update_query.ru
+./generate_update_query.sh $id $fa $te $tr $ti 
+
+## Run the UPDATE query to insert individual into copy ontology
+echo -e "Adding individual MRIO_000$id with \n\tFlip angle=$fa \n\tTE=$te \n\tTR=$tr \n\tTI=$ti"
+./run.sh robot query \
+    -i MRIO-edit.owl \
+    -u ../sparql/update_query.ru \
+    -o MRIO-individuals.owl
+
+## Use reasoner to classify new individual 
+## and query for acquisition type
+echo "Running HermiT reasoner to classify individual..."
+./run.sh robot reason -r hermit \
+    -i MRIO-individuals.owl \
+    --axiom-generators ClassAssertion \
+    -o MRIO-reasoned.owl
+
+echo "Querying for acquisition type of individual..."
+./run.sh robot query \
+    --input MRIO-reasoned.owl \
+    --query ../sparql/individuals.sparql individuals.csv
+
+## Remove temporary files
+echo "Cleaning up..."
+rm MRIO-reasoned.owl MRIO-individuals.owl
+
